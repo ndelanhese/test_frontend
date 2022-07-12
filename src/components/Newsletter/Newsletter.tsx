7237303f130b05fbe7785c812d493be8a4ac5c82
ndelanhese <nnathanh3@gmail.com>
2022-07-12 17:51:03 -0300

    featura - modal init component

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -1,14 +1,61 @@
-import { Button, HStack, Icon, Input, Stack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Icon,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure,
+  ModalFooter,
+} from "@chakra-ui/react";
 import { MdArrowForwardIos } from "react-icons/md";
 
-export function Newsletter(){
+export function Newsletter() {
+
+   const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+  function VerticallyCenter() {
+    
+
     return (
-        <Stack as="form" w="23.375rem" >
-        <Text as="label" fontSize="0.875rem">Inscrevasse na nossa newsletter</Text>
-        <HStack gap="10px" >
+      <>
+        <Button onClick={onOpen}>Trigger modal</Button>
+
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text>Inscrição Realizada com sucesso!</Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Fechar</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
+
+  return (
+    <Stack as="form" w="23.375rem" >
+      <Text as="label" fontSize="0.875rem">
+        Inscrevasse na nossa newsletter
+      </Text>
+      <HStack gap="10px">
         <Input placeholder="Endereço de e-mail" />
-        <Button bg="gray.900" color="white"><Icon as={MdArrowForwardIos}/></Button>
-        </HStack>
-        </Stack>
-        )
+        <Button bg="gray.900" color="white" onClick={onOpen}>
+          <Icon as={MdArrowForwardIos} />
+        </Button>
+      </HStack>
+    </Stack>
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
-import {ChakraProvider} from '@chakra-ui/react'
-import React from 'react'
-import {theme} from '../styles/theme'
-import { MenuDrawerProvider } from '../contexts/MenuDrawerContext';
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import { theme } from "../styles/theme";
+import { MenuDrawerProvider } from "../contexts/MenuDrawerContext";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <MenuDrawerProvider>
-    <Component {...pageProps} />
-    </MenuDrawerProvider>
+        <Component {...pageProps} />
+      </MenuDrawerProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
