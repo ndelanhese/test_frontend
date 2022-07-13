f3673477f6c8bd2f6d696d6f99b95c976063dbef
ndelanhese <nnathanh3@gmail.com>
2022-07-12 22:16:47 -0300

    feature - medical informations with responsiviti

--- src/components/MedicalInformations/MedicalInformation.tsx
+++ src/components/MedicalInformations/MedicalInformation.tsx
@@ -1,8 +1,21 @@
-import { Box, Divider, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Divider,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 
 export function MedicalInformations() {
-  return (
-    <>
+  const isWideVersion = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (!isWideVersion) {
+    return (
       <Stack ml="5rem">
         <Divider w="85vw" mt="3rem" mb="1.5rem" />
 
@@ -27,7 +40,7 @@ export function MedicalInformations() {
           </Stack>
           <Stack gap={1}>
             <Text>Tipo de Sangue</Text>
-            <Text fontSize="1.5rem"> B+</Text>
+            <Text fontSize="1.5rem"> O+</Text>
           </Stack>
           <Stack gap={1}>
             <Text>Doenças</Text>
@@ -45,6 +58,7 @@ export function MedicalInformations() {
             fontWeight={400}
             fontSize="1.875rem"
             mb="1.5rem"
+            mt="2rem"
             lineHeight="35px"
           >
             Contato de emergência
@@ -57,6 +71,46 @@ export function MedicalInformations() {
           </Stack>
         </HStack>
       </Stack>
-    </>
+    );
+  }
+
+  return (
+    <Stack ml="1rem">
+      <Divider w="95vw" mt="3rem" mb="1.5rem" />
+
+      <Stack gap={5}>
+        <Stack gap={1}>
+          <Text>Peso</Text>
+          <Text fontSize="1.5rem">68Kg</Text>
+        </Stack>
+        <Divider w="85vw" mt="3rem" mb="1.5rem" />
+        <Stack gap={1}>
+          <Text>Altura</Text>
+          <Text fontSize="1.5rem">173cm</Text>
+        </Stack>
+        <Divider w="85vw" mt="3rem" mb="1.5rem" />
+        <Stack gap={1}>
+          <Text>Tipo de Sangue</Text>
+          <Text fontSize="1.5rem"> O+</Text>
+        </Stack>
+        <Divider w="85vw" mt="3rem" mb="1.5rem" />
+        <Stack gap={1}>
+          <Text>Doenças</Text>
+          <Text fontSize="1.5rem">Nenhuma</Text>
+        </Stack>
+        <Divider w="85vw" mt="3rem" mb="1.5rem" />
+        <Stack gap={1}>
+          <Text>Alergias</Text>
+          <Text fontSize="1.5rem">Nenhuma</Text>
+        </Stack>
+
+        <Divider w="85vw" mt="3rem" mb="1.5rem" />
+
+        <Stack gap={1}>
+          <Text>Michael Caplan</Text>
+          <Text fontSize="1.5rem"> +1 (650) 555-4703</Text>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
