50dbfbfd14f0d015af8e3fbf262c94f0614c1c8f
ndelanhese <nnathanh3@gmail.com>
2022-07-13 22:31:54 -0300

    feature - links on api connect

--- src/components/SocialNetworks/SocialNetworks.tsx
+++ src/components/SocialNetworks/SocialNetworks.tsx
@@ -9,99 +9,60 @@ import { MdFacebook } from "react-icons/md";
 import { api } from "../../services/api";
 
 export function SocialNetworks() {
-  const [profile, setProfile] = useState<any>({});
+  const [profile, setProfile] = useState([]);
 
   useEffect(() => {
     api
       .get("/profile")
       .then((response) => {
-        setProfile(response.data);
+        setProfile(response.data.social_media_links);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-     
-
   return (
     <HStack mt="1.5rem" gap="0.7rem">
-      <Link as="a" href={"profile.social_media_links"} target="_blank">
-        <Button
-          h={10}
-          w={7}
-          bg="gray.900"
-          color="white"
-          _hover={{
-            background: "white",
-            color: "black",
-            transform: "scale(1.2, 1.2)",
-            transition: "0.5s",
-            border: "1px solid",
-          }}
-        >
-          <Icon as={MdFacebook} w={7} h={7}></Icon>
-        </Button>
-      </Link>
-      <Link
-        as="a"
-        href={"profile.social_media_links"}
-        target="_blank"
-      >
-        <Button
-          h={10}
-          w={7}
-          bg="gray.900"
-          color="white"
-          _hover={{
-            background: "white",
-            color: "black",
-            transform: "scale(1.2, 1.2)",
-            transition: "0.5s",
-            border: "1px solid",
-          }}
-        >
-          <Icon as={AiOutlineInstagram} w={7} h={7}></Icon>
-        </Button>
-      </Link>
-      <Link
-        as="a"
-        href={"profile.social_media_links"}
-        target="_blank"
-      >
-        <Button
-          h={10}
-          w={7}
-          bg="gray.900"
-          color="white"
-          _hover={{
-            background: "white",
-            color: "black",
-            transform: "scale(1.2, 1.2)",
-            transition: "0.5s",
-            border: "1px solid",
-          }}
-        >
-          <Icon as={AiOutlineTwitter} w={7} h={7}></Icon>
-        </Button>
-      </Link>
-      <Link as="a" href={"profile.social_media_links"} target="_blank">
-        <Button
-          h={10}
-          w={7}
-          bg="gray.900"
-          color="white"
-          _hover={{
-            background: "white",
-            color: "black",
-            transform: "scale(1.2, 1.2)",
-            transition: "0.5s",
-            border: "1px solid",
-          }}
-        >
-          <Icon as={AiFillYoutube} w={7} h={7}></Icon>
-        </Button>
-      </Link>
+      {profile.map((item) => {
+        let icon;
+        switch (item.slug) {
+          case "facebook":
+            icon = <Icon as={MdFacebook} w={7} h={7}></Icon>;
+            break;
+            case "instagram":
+            icon = <Icon as={AiOutlineInstagram} w={7} h={7}></Icon>;
+            break;
+            case "twitter":
+            icon =  <Icon as={AiOutlineTwitter} w={7} h={7}></Icon>
+            break;
+            case "youtube":
+            icon = <Icon as={AiFillYoutube} w={7} h={7}></Icon>
+            break;
+          default:
+            break;
+        }
+
+        return (
+          <Link key={item.slug} as="a" href={item.url} target="_blank">
+            <Button
+              h={10}
+              w={7}
+              bg="gray.900"
+              color="white"
+              _hover={{
+                background: "white",
+                color: "black",
+                transform: "scale(1.2, 1.2)",
+                transition: "0.5s",
+                border: "1px solid",
+              }}
+            >
+              {icon}
+            </Button>
+          </Link>
+        );
+      })}
     </HStack>
   );
 }
